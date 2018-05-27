ca9acafead7342278554f44d0fb01753e98d92e8
Kevin McManamon <kevin.mcmanamon@seebyte.com>
2018-05-27 22:57:55 +0100

    added new powershell script to setup cygwin - not working yet

---
+++ cygwin_setup.ps1
@@ -0,0 +1,17 @@
+# Run in Powershell
+#First download the cygwin installer:
+
+$url = "https://cygwin.com/setup-x86_64.exe"
+$file = "C:\Users\$env:UserName\Downloads\setup-x86_64.exe"
+Invoke-WebRequest -Uri $url -OutFile $file
+
+#Install cygwin with the desired packages:
+& C:\Users\$env:UserName\Downloads\setup-x86_64.exe `
+--quiet-mode `
+--packages `
+git, `
+the_silver_searcher, `
+tmux, ` 
+python3,`
+gcc, `
+vim
